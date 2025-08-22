6e676aee9c971c68474a0c87006a0c7d14ebf892
ThaisKheyla <kheyla.paucara@sptech.school>
2025-08-22 13:58:52 -0300

    Alguns exemplos

---
+++ Atividade 01.R
@@ -0,0 +1,18 @@
+#variaveis
+a <- 10
+b <- 2
+c = 1
+# = passagem de um parametro 
+# <- 
+if(g <-4){
+  "teste 1"
+}else{
+  "teste 2"
+}
+
+somar <- function(a, b = 2){
+  return(a + b)
+}
+
+somar(10, 32)
+somar(10)
