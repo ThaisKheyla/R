bb5c1b4cefd9848a1a8d62450437653682e8de3c
ThaisKheyla <kheyla.paucara@sptech.school>
2025-08-25 10:02:36 -0300

    Dataframes

---
+++ datasets/datasetCSV.R
@@ -0,0 +1,18 @@
+df_grupo <- rbind(captura.carlos,
+                  captura.giuliana,
+                  captura.joana,
+                  captura.joana,
+                  captura.marina,
+                  captura.rafael)
+
+
+rm(captura.carlos,
+   captura.giuliana,
+   captura.joana,
+   captura.marina,
+   captura.rafael)
+
+#exibindo os primeiros registros
+head(df_grupo)
+
+str(df_grupo)
